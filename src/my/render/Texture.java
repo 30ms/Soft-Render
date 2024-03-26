759d8d1830bf008540df0f6e28b72dd0accb354d
刘振彬 <1455154800@qq.com>
2024-03-26 14:57:03 +0800

    纹理采样添加最邻近点采样和双线性采样

--- src/my/render/Texture.java
+++ src/my/render/Texture.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 public class Texture<T> {
     int width, height;
     T[][] pixels;
+    Filtering filtering;
 
     public Texture(T[] rawRgbData, int width)
     {
@@ -30,6 +31,7 @@ public class Texture<T> {
             int x = i % width, y = i / width;
             pixels[x][y] = rawRgbData[i];
         }
+        filtering = Filtering.NEAREST;
     }
 
     public T getPixel(int x, int y)
@@ -47,7 +49,53 @@ public class Texture<T> {
         if(v > 1) v = v - (int)v;
         if(v < 0) v = 1 - ((int) v - v);
 
-        return getPixel((int) (u * width % width), (int) (v * height % height));
+        switch (filtering) {
+            case NEAREST:
+                return nearest(u, v);
+            case BILINEAR:
+                return bilinear(u, v);
+            default:
+                throw new RuntimeException("Unknown filtering method");
+        }
+    }
+
+    //最邻近点采样(NEAREST)
+    private T nearest(float u, float v) {
+        int x = (int) (u * (width - 1));
+        int y = (int) (v * (height - 1));
+        return getPixel(x, y);
+    }
+
+    //双线性采样(Bilinear)
+    private T bilinear(float u, float v) {
+        float x = u * (width - 1);
+        float y = v * (height - 1);
+
+        int x0 = (int) x;
+        int y0 = (int) y;
+
+        int x1 = x0 + 1;
+        int y1 = y0 + 1;
+        if(x1 > width -1) x1 = x0;
+        if(y1 > height -1) y1 = y0;
+
+        return interpolate(getPixel(x0, y0), getPixel(x1, y0), getPixel(x0, y1), getPixel(x1, y1), x - x0, y - y0);
+    }
+
+    private T interpolate(T pixel, T pixel1, T pixel2, T pixel3, float v, float v1) {
+        return interpolate(interpolate(pixel, pixel1, v), interpolate(pixel2, pixel3, v), v1);
+    }
+
+    private T interpolate(T pixel, T pixel1, float f) {
+        if (pixel instanceof Vector4f) {
+            Vector4f v1 = (Vector4f) pixel, v2 = (Vector4f) pixel1;
+            return (T) new Vector4f(
+                    v1.X + f * (v2.X - v1.X),
+                    v1.Y + f * (v2.Y - v1.Y),
+                    v1.Z + f * (v2.Z - v1.Z),
+                    v1.W + f * (v2.W - v1.W));
+        }
+        throw new RuntimeException("not support");
     }
 
     public T sample(Vector2f st) {
@@ -66,6 +114,12 @@ public class Texture<T> {
                 .mapToObj(color -> new Vector4f((color >> 16 & 0xFF) / 255f, (color >> 8 & 0xFF) / 255f, (color & 0xFF) / 255f, (color >> 24 & 0xFF) / 255f))
                 .toArray(Vector4f[]::new);
         return new Texture<>(data, w);
+    }
+
+    //纹理过滤
+    public enum Filtering {
+        NEAREST,
+        BILINEAR
     }
 
 }
